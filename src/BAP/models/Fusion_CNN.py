a94ad4ab588b31cb834597d778e2a9a7ada31323
ztutar <zeyneptutar94@gmail.com>
2025-11-17 21:11:42 +0100

    fusion update

--- src/BAP/models/Fusion_CNN.py
+++ src/BAP/models/Fusion_CNN.py
@@ -70,6 +70,8 @@ def build_FusionCNN(
    metaph_input = layers.Input(shape=roi_shape, dtype=tf.float32, name="metaph") # [B,H,W,1]
    carpal_features = ROI_CNN_head(carpal_input, roi_channels, roi_dense_units, "carpal") # [B,roi_dense_units]
    metaph_features = ROI_CNN_head(metaph_input, roi_channels, roi_dense_units, "metaph") # [B,roi_dense_units]
+   carpal_features = layers.Dense(int(roi_dense_units/2), activation="relu", name="carpal_dense")(carpal_features) # [B,roi_dense_units/2]
+   metaph_features = layers.Dense(int(roi_dense_units/2), activation="relu", name="metaph_dense")(metaph_features) # [B,roi_dense_units/2]
 
    fused = layers.Concatenate(name="fusion_concat")([global_features, carpal_features, metaph_features]) # [B, global_dense_units + roi_dense_units*2]
 
